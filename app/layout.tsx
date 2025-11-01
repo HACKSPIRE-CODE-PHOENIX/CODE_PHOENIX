0c0bb9d08a93c97d04257789a8776b35a39110b8
Achinta <achintasiddhanta0304@gmail.com>
2025-11-01 11:27:19 +0530

    till now2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,35 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const montserrat = Montserrat({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700", "300"], // you can customize this
+    variable: "--font-montserrat",
 });
 
 export const metadata: Metadata = {
-  title: "DonorFi",
-  description: "Milestone Based Transparent Donations on Aptos",
+    title: "DonorFi",
+    description: "Milestone Based Transparent Donations on Aptos",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="en">
-      <body className="flex bg-[#0e0e0e] text-white">
-      <div className="fixed top-0 left-0 h-screen w-56">
-          <Sidebar />
-      </div>
-      <main className="flex-1 ml-64 p-6 overflow-y-auto h-screen">
-          {children}
-      </main>
-      </body>
-      </html>
+    return (
+        <html lang="en">
+        <body className={`${montserrat.variable} font-montserrat flex bg-[#0e0e0e] min-h-screen text-white`}>
+            <div className="fixed top-0 left-0 h-screen w-56">
+                <Sidebar />
+            </div>
+            <main className="flex-1 ml-64 p-6 ">
+                {children}
+            </main>
+        </body>
+        </html>
 
-  );
+    );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,62 +1,26 @@
 'use client';
 
-import CoinCard from "@/components/CoinCard";
-import { beneficiaryData, causeData, coincard, data } from "@/constants";
-import { DataTable } from "@/components/data-table";
-import { beneficiaryColumns, causeColumns, columns } from "@/components/columns";
+import { useState } from "react";
+import YourDonation from "@/components/YourDonation";
+import YourFunds from "@/components/YourFunds";
 
 export default function Home() {
+
+    const [isOpen, setIsOpen] = useState(true);
+
     return (
-        <div className="px-12 py-8 text-white bg-[#0e0e0e]">
+        <div className="px-12 py-1 text-white ml-8">
             {/* Heading */}
             <div>
-                <h2 className="text-3xl font-semibold">Welcome back, John</h2>
+                <h2 className="text-3xl mt-2 font-semibold">Welcome back, John</h2>
                 <p className="font-light text-sm opacity-80 mt-2">
                     Donate with Trust. Impact with Transparency.
                 </p>
             </div>
 
-            {/* Coin Cards */}
-            <div className="flex flex-wrap gap-6 mt-8">
-                {coincard.map((coin) => (
-                    <CoinCard
-                        key={coin.id}
-                        title={coin.title}
-                        description={coin.description}
-                        buttonText={coin.buttonText}
-                    />
-                ))}
-            </div>
+            <YourDonation />
 
-            {/* Tables Section */}
-            <div className="mt-10 flex flex-wrap gap-4">
-                {/* Left Section (Recent Transactions) */}
-                <div className="flex">
-                    <DataTable
-                        width={625}
-                        title="Recent Transactions"
-                        columns={columns}
-                        data={data}
-                        button={true}
-                    />
-                </div>
-
-                {/* Right Section (Top Causes + Beneficiaries stacked) */}
-                <div className="flex flex-col gap-4">
-                    <DataTable
-                        width={292}
-                        title="Top Causes You Support"
-                        columns={causeColumns}
-                        data={causeData}
-                    />
-                    <DataTable
-                        width={292}
-                        title="Your Favorite Beneficiaries"
-                        columns={beneficiaryColumns}
-                        data={beneficiaryData}
-                    />
-                </div>
-            </div>
+            <YourFunds />
 
         </div>
     );
